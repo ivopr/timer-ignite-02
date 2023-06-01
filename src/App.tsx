730f2980b2db4cb57725e964f3cf26027ec388f0
Ivo <ivoprovensi1@gmail.com>
2023-06-01 15:47:49 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
+  )
+}
